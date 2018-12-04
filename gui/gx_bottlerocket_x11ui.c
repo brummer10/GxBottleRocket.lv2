5fc2282e20230d71b95d3046e934ee1f8b11ed6a
brummer10 <brummer-@web.de>
2018-12-04 13:01:06 +0100

    fix keyboard event handling

--- gui/gx_bottlerocket_x11ui.c
+++ gui/gx_bottlerocket_x11ui.c
@@ -211,11 +211,11 @@ static LV2UI_Handle instantiate(const struct _LV2UI_Descriptor * descriptor,
 		return NULL;
 	}
 
-	ui->controls[0] = { 1.0, 1.0, 0.0, 1.0, 1.0, 50, 60, 61, 61, false,"POWER", BSWITCH, BYPASS};
-	ui->controls[1] = { 0.5, 0.5, 0.0, 1.0, 0.01, 120, 60, 61, 61, false,"GAIN", KNOB, GAIN};
-	ui->controls[2] = { 0.5, 0.5, 0.0, 1.0, 0.01, 190, 60, 61, 61, false,"BASS", KNOB, BASS};
-	ui->controls[3] = { 0.5, 0.5, 0.0, 1.0, 0.01, 260, 60, 61, 61, false,"TREBLE", KNOB, TREBLE};
-	ui->controls[4] = { 0.5, 0.5, 0.0, 1.0, 0.01, 330, 60, 61, 61, false,"VOLUME", KNOB, VOLUME};
+	ui->controls[0] = (gx_controller) {{ 1.0, 1.0, 0.0, 1.0, 1.0},{ 50, 60, 61, 61}, false,"POWER", BSWITCH, BYPASS};
+	ui->controls[1] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01},{ 120, 60, 61, 61}, false,"GAIN", KNOB, GAIN};
+	ui->controls[2] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01},{ 190, 60, 61, 61}, false,"BASS", KNOB, BASS};
+	ui->controls[3] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01},{ 260, 60, 61, 61}, false,"TREBLE", KNOB, TREBLE};
+	ui->controls[4] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01},{ 330, 60, 61, 61}, false,"VOLUME", KNOB, VOLUME};
 
 
 	ui->pedal = cairo_image_surface_create_from_stream(ui, LDVAR(pedal_png));
@@ -411,7 +411,20 @@ static void knob_expose(gx_bottlerocketUI *ui,gx_controller* knob) {
 	cairo_pattern_destroy (pat);
 
 	/** show label below the knob**/
-	cairo_set_source_rgba (ui->crf, 0.6, 0.6, 0.6,0.6);
+	/** show label below the knob**/
+	if (knob->is_active) {
+		cairo_set_source_rgba (ui->crf, 0.8, 0.8, 0.8,0.8);
+	} else {
+		cairo_set_source_rgba (ui->crf, 0.6, 0.6, 0.6,0.6);
+	}
+	cairo_set_font_size (ui->crf, 12.0);
+	cairo_select_font_face (ui->crf, "Sans", CAIRO_FONT_SLANT_NORMAL,
+							   CAIRO_FONT_WEIGHT_BOLD);
+	cairo_text_extents(ui->crf,knob->label , &extents);
+
+	cairo_move_to (ui->crf, knobx1-extents.width/2, grow+6+extents.height);
+	cairo_show_text(ui->crf, knob->label);
+	cairo_new_path (ui->crf);
 	cairo_set_font_size (ui->crf, 12.0);
 	cairo_select_font_face (ui->crf, "Sans", CAIRO_FONT_SLANT_NORMAL,
 							   CAIRO_FONT_WEIGHT_BOLD);
@@ -427,12 +440,6 @@ static void bypass_expose(gx_bottlerocketUI *ui, gx_controller* switch_) {
 	cairo_set_operator(ui->crf,CAIRO_OPERATOR_CLEAR);
 	cairo_paint(ui->crf);
 	cairo_set_operator(ui->crf,CAIRO_OPERATOR_OVER);
-	if (switch_->is_active == true) {
-		cairo_set_source_rgba (ui->crf, 0.8, 0.8, 0.8,0.8);
-		cairo_rectangle (ui->crf,6,6,49,48);
-		cairo_fill(ui->crf);
-		cairo_stroke(ui->crf);
-	}
 	if(switch_->adj.value > 0.9)
 		cairo_set_source_surface (ui->crf, ui->pswitch_on, 0, 0);
 	else
@@ -440,7 +447,11 @@ static void bypass_expose(gx_bottlerocketUI *ui, gx_controller* switch_) {
 	cairo_paint (ui->crf);
 	/** show label below the switch**/
 	cairo_text_extents_t extents;
-	cairo_set_source_rgba (ui->crf, 0.6, 0.6, 0.6,0.6);
+	if (switch_->is_active) {
+		cairo_set_source_rgba (ui->crf, 0.8, 0.8, 0.8,0.8);
+	} else {
+		cairo_set_source_rgba (ui->crf, 0.6, 0.6, 0.6,0.6);
+	}
 	cairo_set_font_size (ui->crf, 12.0);
 	cairo_select_font_face (ui->crf, "Sans", CAIRO_FONT_SLANT_NORMAL,
 							   CAIRO_FONT_WEIGHT_BOLD);
@@ -743,6 +754,46 @@ static void motion_event(gx_bottlerocketUI *ui, double start_value, XMotionEvent
 	
 }
 
+// map supported key's to integers or return zerro
+static int key_mapping(Display *dpy, XKeyEvent *xkey) {
+	if (xkey->keycode == XKeysymToKeycode(dpy,XK_Tab))
+		return (xkey->state == ShiftMask) ? 1 : 2;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Up))
+		return 3;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Right))
+		return 3;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Down))
+		return 4;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Left))
+		return 4;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Home))
+		return 5;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Insert))
+		return 6;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_End))
+		return 7;
+	// keypad
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Subtract))
+		return 1;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Add))
+		return 2;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Up))
+		return 3;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Right))
+		return 3;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Down))
+		return 4;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Left))
+		return 4;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Home))
+		return 5;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Insert))
+		return 6;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_End))
+		return 7;
+	else return 0;
+}
+
 // general xevent handler
 static void event_handler(gx_bottlerocketUI *ui) {
 	XEvent xev;
@@ -802,26 +853,26 @@ static void event_handler(gx_bottlerocketUI *ui) {
 			break;
 
 			case KeyPress:
-				if ((xev.xkey.state == ShiftMask) &&
-				  (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Tab)))
-					set_previous_controller_active(ui);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Tab))
-					set_next_controller_active(ui);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Up))
-					key_event(ui, 1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Right))
-					key_event(ui, 1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Down))
-					key_event(ui, -1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Left))
-					key_event(ui, -1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Home))
-					set_key_value(ui, 1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Insert))
-					set_key_value(ui, 2);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_End))
-					set_key_value(ui, 3);
+				switch (key_mapping(ui->dpy, &xev.xkey)) {
+					case 1: set_previous_controller_active(ui);
+					break;
+					case 2: set_next_controller_active(ui);
+					break;
+					case 3: key_event(ui, 1);
+					break;
+					case 4: key_event(ui, -1);
+					break;
+					case 5: set_key_value(ui, 1);
+					break;
+					case 6: set_key_value(ui, 2);
+					break;
+					case 7: set_key_value(ui, 3);
+					break;
+					default:
+					break;
+				}
 			break;
+
 			case EnterNotify:
 				if (!blocked) get_active_controller(ui, true);
 			break;
@@ -830,7 +881,7 @@ static void event_handler(gx_bottlerocketUI *ui) {
 			break;
 			case MotionNotify:
 				// mouse move while button1 is pressed
-				if(xev.xmotion.state == Button1MotionMask) {
+				if(xev.xmotion.state & Button1Mask) {
 					motion_event(ui, start_value, &xev.xmotion);
 				}
 			break;
@@ -880,7 +931,6 @@ static const LV2UI_Descriptor descriptor = {
 	extension_data
 };
 
-extern "C"
 LV2_SYMBOL_EXPORT
 const LV2UI_Descriptor* lv2ui_descriptor(uint32_t index) {
 	switch (index) {
